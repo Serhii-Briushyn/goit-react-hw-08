9ea477d3ff9b138af211e6ec4cb8a2e16c4700f6
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-10-19 15:20:31 +0200

    baseURL

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+export const goItApi = axios.create({
+  baseURL: "https://connections-api.goit.global/",
+});
 
 const setAuthHeader = (token) => {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  goItApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = "";
+  goItApi.defaults.headers.common.Authorization = "";
 };
 
 export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/users/signup", credentials);
+      const res = await goItApi.post("/users/signup", credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -33,7 +35,7 @@ export const logIn = createAsyncThunk(
   "auth/login",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/users/login", credentials);
+      const res = await goItApi.post("/users/login", credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -47,7 +49,7 @@ export const logIn = createAsyncThunk(
 
 export const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
-    await axios.post("/users/logout");
+    await goItApi.post("/users/logout");
     clearAuthHeader();
   } catch {
     return thunkAPI.rejectWithValue("Logout failed. Please try again.");
@@ -65,7 +67,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       setAuthHeader(persistedToken);
-      const res = await axios.get("/users/current");
+      const res = await goItApi.get("/users/current");
       return res.data;
     } catch {
       return thunkAPI.rejectWithValue(
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+import { goItApi } from "../auth/operations";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
+      const response = await goItApi.get("/contacts");
       return response.data;
     } catch {
       return thunkAPI.rejectWithValue(
@@ -21,7 +19,7 @@ export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post("/contacts", newContact);
+      const response = await goItApi.post("/contacts", newContact);
       return response.data;
     } catch {
       return thunkAPI.rejectWithValue(
@@ -35,7 +33,7 @@ export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (contactId, thunkAPI) => {
     try {
-      await axios.delete(`/contacts/${contactId}`);
+      await goItApi.delete(`/contacts/${contactId}`);
       return contactId;
     } catch {
       return thunkAPI.rejectWithValue(
@@ -49,7 +47,7 @@ export const updateContact = createAsyncThunk(
   "contacts/updateContact",
   async ({ id, updatedContact }, thunkAPI) => {
     try {
-      const response = await axios.patch(`/contacts/${id}`, updatedContact);
+      const response = await goItApi.patch(`/contacts/${id}`, updatedContact);
       return response.data;
     } catch {
       return thunkAPI.rejectWithValue(
