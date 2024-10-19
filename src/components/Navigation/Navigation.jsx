fc8ff74628f8026bc8dbc851d76445c47c7b12a4
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-10-19 16:06:42 +0200

    Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,9 +3,12 @@ import css from "./Navigation.module.css";
 import clsx from "clsx";
 import { useState } from "react";
 import { RiMenu2Fill, RiCloseFill } from "react-icons/ri";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 export const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const buildLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.activeLink);
@@ -17,32 +20,46 @@ export const Navigation = () => {
 
   return (
     <nav className={css.wrapper}>
-      <div className={css.menuDesktop}>
+      {isLoggedIn && (
+        <>
+          <div className={css.menuDesktop}>
+            <NavLink className={buildLinkClass} to="/">
+              Home
+            </NavLink>
+
+            <NavLink className={buildLinkClass} to="/contacts">
+              Contacts
+            </NavLink>
+          </div>
+
+          <button className={css.button} onClick={toggleMenu}>
+            <RiMenu2Fill />
+          </button>
+
+          <div className={clsx(css.menuModal, isMenuOpen && css.open)}>
+            <button className={css.closeButton} onClick={toggleMenu}>
+              <RiCloseFill />
+            </button>
+            <NavLink className={buildLinkClass} to="/" onClick={toggleMenu}>
+              Home
+            </NavLink>
+
+            <NavLink
+              className={buildLinkClass}
+              to="/contacts"
+              onClick={toggleMenu}
+            >
+              Contacts
+            </NavLink>
+          </div>
+        </>
+      )}
+
+      {!isLoggedIn && (
         <NavLink className={buildLinkClass} to="/">
           Home
         </NavLink>
-
-        <NavLink className={buildLinkClass} to="/contacts">
-          Contacts
-        </NavLink>
-      </div>
-
-      <button className={css.button} onClick={toggleMenu}>
-        <RiMenu2Fill />
-      </button>
-
-      <div className={clsx(css.menuModal, isMenuOpen && css.open)}>
-        <button className={css.closeButton} onClick={toggleMenu}>
-          <RiCloseFill />
-        </button>
-        <NavLink className={buildLinkClass} to="/" onClick={toggleMenu}>
-          Home
-        </NavLink>
-
-        <NavLink className={buildLinkClass} to="/contacts" onClick={toggleMenu}>
-          Contacts
-        </NavLink>
-      </div>
+      )}
     </nav>
   );
 };
